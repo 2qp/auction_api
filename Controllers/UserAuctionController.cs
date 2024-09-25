a87028422d89ffc901e75a820b91aef147c23cf6
2qp <56475797+2qp@users.noreply.github.com>
2024-09-25 21:49:07 +0530

    fix: replace ContinueWith with await in GetUserAuction and GetUserAuctions methods

--- Controllers/UserAuctionController.cs
+++ Controllers/UserAuctionController.cs
@@ -9,24 +9,25 @@ namespace auction_api.Controllers;
 [Route("api/[controller]")]
 public class UserAuctionController(IUserAuctionService UserAuctionService) : ControllerBase
 {
-    public async Task<ActionResult<UserAuction>> GetUserAuction([FromRoute] string userId, [FromRoute] string auctionId) =>
-       await UserAuctionService.GetUserAuctionAsync(userId, auctionId).ContinueWith(t =>
-        {
-            if (t.IsFaulted) return StatusCode(500, "Internal server error");
-
-            var auction = t.Result;
-            if (auction == null) NotFound();
-
-            return Ok(t.Result);
-        });
-
     [HttpGet]
+    public async Task<ActionResult<UserAuction>> GetUserAuction([FromRoute] string userId, [FromRoute] string auctionId) =>
+    await UserAuctionService.GetUserAuctionAsync(userId, auctionId) is UserAuction auction
+        ? Ok(auction)
+        : NotFound();
+
+    [HttpGet("UserAuctions")]
     public async Task<ActionResult<IEnumerable<UserAuction>>> GetUserAuctions([FromRoute] string userId) =>
-       await UserAuctionService.GetUserAuctionsAsync(userId).ContinueWith(t => Ok(t.Result));
+    await UserAuctionService.GetUserAuctionsAsync(userId) is var auctions
+       ? Ok(auctions)
+       : NotFound();
 
     [HttpPost]
-    public async Task<ActionResult> AddUserAuction(UserAuction entity) =>
-       await UserAuctionService.AddUserAuctionAsync(entity).ContinueWith(t => CreatedAtAction(nameof(GetUserAuction), new { userId = entity.UserId, auctionId = entity.AuctionId }, entity));
+    public async Task<ActionResult> AddUserAuction(string groupId, string connectionId, UserAuction entity)
+    {
+        await UserAuctionService.JoinAuctionAsync(groupId, connectionId, entity);
+        return CreatedAtAction(nameof(GetUserAuction), new { userId = entity.UserId, auctionId = entity.AuctionId }, entity);
+    }
+
 
     [HttpPut]
     public async Task<ActionResult> UpdateUserAuction(UserAuction entity) =>
